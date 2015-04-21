7c41111245fb911e006e37c9420b944a495c1bb3
shunie99 <astroboy1985_28@hotmail.com>
2015-04-21 06:01:06 +0000

    Make a basic User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root  'static_pages#home'
   match '/about',   to: 'static_pages#about',   via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
